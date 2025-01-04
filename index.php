7eb850e876fbacf4d50a350a1365f452a3947609
Suminan02 <talalajantawong@gmail.com>
2025-01-04 13:42:46 +0700

    php var

--- index.php
+++ index.php
@@ -7,7 +7,15 @@
 </head>
 <body>
   <?php
-    echo"hello world!";
+  $titlename = "รายรับ-รายจ่าย";
+  $income=50000;
+  $expense=20000;
+
+  echo $titlename."<br>";
+  echo "income =".$income."<br>";
+  echo "expense =".$expense."<br>";
+  echo $income-$expense."<br>";
+
   ?>
 </body>
 </html>
