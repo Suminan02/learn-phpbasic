de00d514762497ed20c004557857398b8bcf5345
Suminan02 <talalajantawong@gmail.com>
2025-01-04 10:30:58 +0700

    hello world

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
-<?php
-  echo"hello world <br>";
-  echo "hello world 2";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PPHP & HTML</title>
+</head>
+<body>
+  <?php
+    echo"hello world!";
+  ?>
+</body>
+</html>
