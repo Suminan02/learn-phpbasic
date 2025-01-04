19f9c86615d38e288e4cbf53ce94bf7c5630a3b1
Suminan02 <talalajantawong@gmail.com>
2025-01-04 18:41:10 +0700

    php data type

---
+++ datatype.php
@@ -0,0 +1,19 @@
+
+
+  <?php
+  //integer = 1,2,3...
+  //float/double=1.00,2.00
+  //biilean=true,false
+  //string="jantawong"
+  //array=[]
+  //object=()
+
+  //gettype()
+
+  $apple=30;
+  $price=0.9;
+  $nameOfFruit="apple";
+  $isFruit=true;
+
+  echo $isFruit;
+  ?>
