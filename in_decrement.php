01cb64ff4d42b653a36cccc97dca2fc2ae42a200
Suminan02 <talalajantawong@gmail.com>
2025-01-05 19:42:46 +0700

    postfix and prefix

---
+++ in_decrement.php
@@ -0,0 +1,23 @@
+<?php
+  $a=5;
+  $b= 10;
+
+  // increase or add
+  echo"before increase with ++ prefix =".$a."<br>";
+  echo "after us ++ prefix =". ++$a."<br>";
+  echo "present value of a =".$a."<br><hr>";
+
+  echo "postfix".$a."<br>";
+  echo $a++."<br>";
+  echo $a."<br><hr>";
+
+  echo"before decrease with -- prefix =".$b."<br>";
+  echo "after us ++ prefix =". --$b."<br>";
+  echo "present value of a =".$b."<br><hr>";
+
+  echo "postfix".$b."<br>";
+  echo $b--."<br>";
+  echo $b."<br><hr>";
+
+
+?>
---
+++ stringoperator.php
@@ -0,0 +1,8 @@
+<?php
+  $name="test";
+  echo $name."string<br>";
+  echo $name;
+  $name.="string<br>";
+  echo $name;
+  echo $name;
+?>
